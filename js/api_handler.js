1497e962ccb367d2b739f9378a84043397dcff8b
lukeportanier <80636466+lukeportanier@users.noreply.github.com>
2023-04-08 18:28:51 +0200

    Update api_handler.js

--- js/api_handler.js
+++ js/api_handler.js
@@ -2,9 +2,13 @@ var api_url = 'https://api-dot-cloud-test-1232.ew.r.appspot.com/'
 
 function GenerateNumbers()
 {
-    fetch(api_url+'GenerateNumbers')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    fetch(api_url+'GenerateNumbers', {
+        method: 'GET',
+        mode: 'no-cors'
+    })
+        .then(response => {
+    })
+        .catch(error => {
+    });
 }
     
